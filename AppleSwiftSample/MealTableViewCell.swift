a3fa5c9ed1a7feb4b41488f48bd4f92ce49acbb4
Jack <rainbowlight1998@gmail.com>
2016-10-10 23:32:45 +0900

    TableView

---
+++ AppleSwiftSample/MealTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  MealTableViewCell.swift
+//  AppleSwiftSample
+//
+//  Created by Ka on 2016/10/10.
+//  Copyright © 2016年 KaJack. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
